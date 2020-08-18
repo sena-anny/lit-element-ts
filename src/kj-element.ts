4a34f43f7f5fa3627988253c089f8578877b8e54
KJ <cozykoji18@gmail.com>
2020-08-19 00:00:33 +0900

    slot setting

--- src/kj-element.ts
+++ src/kj-element.ts
@@ -9,9 +9,20 @@ export class KjElement extends LitElement {
     div {
       color: red;
     }
+    :host {
+      padding: 50px;
+    }
+    ::slotted(p) {
+      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+        Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+    div ::slotted(*) {
+      color: green;
+    }
   `;
 
   render() {
-    return html`<div>Hello KJ ${this.message} <slot name="two"></slot></div>`;
+    return html`<slot></slot>
+      <div>Hello KJ ${this.message} <slot name="two"></slot></div>`;
   }
 }
